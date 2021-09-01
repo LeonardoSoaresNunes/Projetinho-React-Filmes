e8cf2783461879e67cfbe6030f73c6a224ff9679
LeonardoSoaresNunes <leonardoeaf2009@hotmail.com>
2021-09-01 16:29:40 -0300

    Conectanto com o Back And Api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
-const Home = () =>{
+class  Home extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            filmes:[]
+
+        }
+        this.loadFilmes = this.loadFilmes.bind(this);
+    }
+
+    componentDidMount(){
+       this.loadFilmes();
+    }
+
+    loadFilmes(){
+      //https://sujeitoprogramador.com/r-api/?api=filmes
+      let url = 'https://sujeitoprogramador.com/r-api/?api=filmes';
+      fetch(url)
+      .then((r)=> r.json())
+      .then((json)=>{
+          this.setState({filmes: json});
+          console.log(json);
+
+      });
+
+
+    }
+
+    render(){
     return(
 
         <h1>Bem Vindo a Pagina Home</h1>
 
     );
 }
-
+}
 export default Home;
